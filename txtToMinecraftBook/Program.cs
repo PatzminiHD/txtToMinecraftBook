45d64d4dffc9f3340a134c1ec8f32c4a91360cc0
PatzminiHD <91912090+PatzminiHD@users.noreply.github.com>
2023-08-19 13:39:58 +0200

    Add Method to store settings

--- txtToMinecraftBook/Program.cs
+++ txtToMinecraftBook/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
 
-namespace txtToMinecraftBook
+namespace txtToMinecraftBook;
+internal static class Program
 {
-    internal static class Program
+    private static Settings _settings = new Settings();
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var loadedSettings = _settings.Load();
+        
+        if (loadedSettings != null)
         {
-            Console.Write("Initial Commit");
+            _settings = loadedSettings;
+        }
+        else
+        {
+            Console.WriteLine("Error loading settings.\nDo you want to initialise with default settings? [Y/n] ");
+            while (true)
+            {
+                var line = Console.ReadLine();
+                if (line is "Y" or "")
+                {
+                    Console.WriteLine(_settings.Save()
+                        ? "Settings file initialised"
+                        : "Error initialising settings file");
+                    break;
+                }
+                if (line == "N")
+                {
+                    break;
+                }
+                Console.WriteLine("[Y/n] ");
+            }
         }
     }
 }
--- txtToMinecraftBook/Settings.cs
+++ txtToMinecraftBook/Settings.cs
@@ -1,6 +1,50 @@
+using System;
+using System.IO;
+using System.Xml.Serialization;
+
 namespace txtToMinecraftBook;
 
 public class Settings
 {
-    public string Version = "v0.0.0";
+    private readonly string _settingsFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "txtToMinecraftBook", "Settings.xml");
+
+    public ProgramInfo programInfo = new();
+    public class ProgramInfo
+    {
+        public string Version = "v0.0.1";
+    }
+
+    public bool Save()
+    {
+        try
+        {
+            var fullName = Directory.GetParent(_settingsFilePath)?.FullName;
+            if (fullName != null)
+                Directory.CreateDirectory(fullName);
+            using var writer = new StreamWriter(_settingsFilePath);
+            var xmlSerializer = new XmlSerializer(this.GetType());
+            xmlSerializer.Serialize(writer, this);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Unable to save the Settings:\n");
+            Console.WriteLine(e);
+            return false;
+        }
+    }
+
+    public Settings? Load()
+    {
+        try
+        {
+            using var reader = new StreamReader(_settingsFilePath);
+            var xmlSerializer = new XmlSerializer(this.GetType());
+            return (Settings)xmlSerializer.Deserialize(reader);
+        }
+        catch
+        {
+            return null;
+        }
+    }
 }
